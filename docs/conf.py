cc23d483494c9a0768d7b46b5ca793712e354f9e
andres-fr <fr_andres@msn.com>
2019-02-24 09:27:55 +0100

    rebuilt docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@ copyright = '2019, Dummy Dumson'
 author = 'Dummy Dumson'
 
 # The short X.Y version
-version = '0.9.0'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.1'
 
 
 # -- General configuration ---------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. dummypackage documentation master file, created by
-   sphinx-quickstart on Sun Feb 24 09:25:21 2019.
+   sphinx-quickstart on Sun Feb 24 09:27:26 2019.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
