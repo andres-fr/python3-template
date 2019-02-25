52c2819eba7103e6cae489da265e90c8e736f044
andres-fr <fr_andres@msn.com>
2019-02-25 07:17:10 +0100

    Bump version: 1.0.3 → 1.0.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def setup():
     #
     setuptools.setup(
         name="dummypackage-dummyname",
-        version="1.0.3",
+        version="1.0.4",
         author="Dummy Name",
         author_email="dd@dummysolutions.com",
         description="A dummy package",
