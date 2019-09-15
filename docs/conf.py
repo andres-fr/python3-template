87b20a51b3994830c4b2ddf7e3b790f17d4a5c1c
andres-fr <fr_andres@msn.com>
2019-09-15 22:57:04 +0200

    Bump version: 1.5.1 → 1.5.2

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2019, Dummy Dumson'
 author = 'Dummy Dumson'
 
 # The short X.Y version
-version = '1.5.1'
+version = '1.5.2'
 # The full version, including alpha/beta/rc tags
-release = '1.5.1'
+release = '1.5.2'
 
 
 # -- General configuration ---------------------------------------------------
